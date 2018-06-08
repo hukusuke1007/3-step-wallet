1bad14a0be26348fa6691238ddb23c8ad164308a
kazumasa <kazumasa.matsumoto62@gmail.com>
2018-06-09 00:21:35 +0900

    update easy-wallet
    アップデートした

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7add9c452573b0ffa1b41d2e65c0e9b1.js"
+  "./precache-manifest.341932d9b0c450ca53e18843b71153c9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "easy-wallet"});
@@ -30,15 +30,19 @@ workbox.clientsClaim();
 self.__precacheManifest = [
   {
     "url": "index.html",
-    "revision": "8a168510f491b53747a1c4fddad648fb"
+    "revision": "d9889f252f5b7fee3770975f0fb2ee71"
   },
   {
     "url": "precache-manifest.7270771877e1a176af0745d4ea731805.js",
     "revision": "7270771877e1a176af0745d4ea731805"
   },
   {
+    "url": "precache-manifest.7add9c452573b0ffa1b41d2e65c0e9b1.js",
+    "revision": "7add9c452573b0ffa1b41d2e65c0e9b1"
+  },
+  {
     "url": "service-worker.js",
-    "revision": "358fefdec7761bd03322d896e733425e"
+    "revision": "9adb4827670a787d1893336f82537808"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
